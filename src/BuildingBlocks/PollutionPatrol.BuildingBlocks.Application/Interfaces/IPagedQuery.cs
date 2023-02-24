e68904ced095bbd6026a2af4efb6d0cff7bfb047
Ilya <ShigeraIlia@gmail.com>
2023-02-24 15:34:01 +0100

    BuildongBlocks: removed unnecessary interfaces
    Mediator provides same interfaces.

--- src/BuildingBlocks/PollutionPatrol.BuildingBlocks.Application/Interfaces/IPagedQuery.cs
+++ src/BuildingBlocks/PollutionPatrol.BuildingBlocks.Application/Interfaces/IPagedQuery.cs
@@ -1,7 +1,11 @@
 namespace PollutionPatrol.BuildingBlocks.Application.Interfaces;
 
+/// <summary>
+/// Marker
+/// </summary>
 public interface IPagedQuery
 {
-    int Page { get; } 
-    int Size { get; }
+    int? Page { get; }
+    int? Size { get; }
 }
+
